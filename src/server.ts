02be1bf850762c5e540d3f6eb3aff8c06b2b8b01
Roberto <roberto@3035tech.com>
2022-05-12 00:17:49 -0300

    feat: nodemailer

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import { prisma } from './prisma'
+import nodemailer from 'nodemailer'
 
 const app = express()
 
 app.use(express.json())
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "e2f10fdce2e864",
+    pass: "9961eff04e541a"
+  }
+});
 
 app.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot} = req.body
@@ -14,6 +24,18 @@ app.post('/feedbacks', async (req, res) => {
       comment,
       screenshot,
     }
+  })
+
+  await transport.sendMail({
+    from: 'Equipe Feedget <oi@feedget.com>',
+    to: 'Roberto Junior <beto.rodriguesds@gmail.com>',
+    subject: 'Novo feedback',
+    html: [
+      `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentario: ${comment}</p>`,
+      `</div>`,
+    ].join('\n')
   })
 
   return res.status(201).json({ data: feedback })
